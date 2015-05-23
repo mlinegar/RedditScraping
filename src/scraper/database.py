721977301964c1a377f67dc51e6e06d403112874
mlinegar <mitchelllinegar@gmail.com>
2015-05-23 15:23:10 -0700

    Removed redundant folders

--- src/scraper/database.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('sqlite:///test.db')
-Session = sessionmaker()
-Base = declarative_base(bind=engine)
